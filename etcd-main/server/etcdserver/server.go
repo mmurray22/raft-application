dcc2e83c29b9b4e9667c693cffdad89cb701040d
astrobliss <reginaldfrank77@berkeley.edu>
2024-11-15 00:36:41 +0000

    write txns to scrooge

--- etcd-main/server/etcdserver/server.go
+++ etcd-main/server/etcdserver/server.go
@@ -1861,7 +1861,7 @@ func (s *EtcdServer) apply(
 
 			//@ethan passes data to go rountine that handles writing to Scrooge
 
-			// s.WriteScroogeC <- e.Data
+			s.WriteScroogeC <- e.Data
 
 			println("!!!!!!!!!", txnCounter)
 			if txnCounter == 0 {
