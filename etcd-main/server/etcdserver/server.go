ee184f79cd6920068e01f143bfa23fe0bf114a0a
astrobliss <reginaldfrank77@berkeley.edu>
2024-11-22 23:20:06 +0000

    dont write to scrooge and log txns

--- etcd-main/server/etcdserver/server.go
+++ etcd-main/server/etcdserver/server.go
@@ -308,6 +308,7 @@ type EtcdServer struct {
 	WriteScroogeC chan []byte
 	drSender      bool
 	ccfSender     bool
+	numTxns       int
 }
 
 // NewServer creates a new EtcdServer from the supplied configuration. The
@@ -355,6 +356,7 @@ func NewServer(cfg config.ServerConfig, drSender bool, ccfSender bool) (srv *Etc
 
 		drSender:  drSender,
 		ccfSender: ccfSender,
+		numTxns:   0,
 	}
 	serverID.With(prometheus.Labels{"server_id": b.cluster.nodeID.String()}).Set(1)
 	srv.cluster.SetVersionChangedNotifier(srv.clusterVersionChanged)
@@ -1978,7 +1980,11 @@ func (s *EtcdServer) applyEntryNormal(e *raftpb.Entry) {
 			isUsefulForDr := raftReq.Range == nil
 			if isUsefulForDr {
 				// send txn to scrooge!
-				s.WriteScroogeC <- e.Data
+				// s.WriteScroogeC <- e.Data
+				s.numTxns += 1
+				if s.numTxns%50000 == 0 {
+					println("NUMTXNS: ", s.numTxns)
+				}
 			}
 		} else if s.ccfSender {
 			isPutTxn := raftReq.Put != nil
@@ -2001,7 +2007,7 @@ func (s *EtcdServer) applyEntryNormal(e *raftpb.Entry) {
 			}
 		} else {
 			// Running raft with scrooge, but no application
-			s.WriteScroogeC <- e.Data
+			// s.WriteScroogeC <- e.Data
 		}
 		ar = s.uberApply.Apply(&raftReq, shouldApplyV3)
 	}
